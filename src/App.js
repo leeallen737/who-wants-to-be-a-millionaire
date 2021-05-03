0e36a28bbc08da341568e53285c63bf2481d7962
Lee Allen <leeallen737@gmail.com>
2021-05-03 15:47:26 +0100

    added sass and creating basic html/css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-   <div>Who want to be a millionaire</div>
+    <div id="container">
+      <section id='top-section'>
+      
+      </section>
+   <h2 id="question">In which part of the body would you find the Patella?</h2>
+   <div id="answers-container">
+    <div id="a" className="answers">Knee</div>
+    <div id="b" className="answers">Head</div>
+    <div id="c" className="answers">Arm</div>
+    <div id="" className="answers">Foot</div>
+   </div>
+   </div>
   );
 }
 
