052ae74914a91dcc024aabdd549e47591f8cea51
Lee Allen <leeallen737@gmail.com>
2021-05-09 13:34:26 +0100

    added correct answer logic

--- src/App.js
+++ src/App.js
@@ -5,40 +5,47 @@ import TRIVIA_DATA from './trivia.data'
 import Answer from './answer/answer.component'
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       visibility: null,
       trivia: TRIVIA_DATA,
       answer2: 'Head',
       answer3: 'Arm',
-      answer4: 'Foot'
+      answer4: 'Foot',
+      
     }
+    
   }
+
+  
 
   componentDidMount() {
     setTimeout(function() { //Start the timer
         this.setState({visibility: 'visible'}) //After 4 seconds, set render to true
     }.bind(this), 4000)
+    
   }
   
+
+
+
   render() {
-   
+    
    return (
     <div id="container">
       <section id='top-section'>
-      
       </section>
 
 
    <h2 id="question"><p style={{visibility: this.state.visibility}}>{this.state.trivia[0].question}</p></h2>
    
    <div id="answers-container">
-   <Answer id='a' key={1}  myAnswer1={this.state.trivia[0].a}/>
-   <Answer id='b'key={2} myAnswer1={this.state.trivia[0].b}/>
-   <Answer id='c' key={3}myAnswer1={this.state.trivia[0].c}/>
-   <Answer id='d' key={4} myAnswer1={this.state.trivia[0].d}/>
+   <Answer id='a' correctAnswer={this.state.trivia[0].correct} key={1} myAnswer={this.state.trivia[0].a}/>
+   <Answer id='b' correctAnswer={this.state.trivia[0].correct} key={2} myAnswer={this.state.trivia[0].b}/>
+   <Answer id='c' correctAnswer={this.state.trivia[0].correct} key={3} myAnswer={this.state.trivia[0].c}/>
+   <Answer id='d' correctAnswer={this.state.trivia[0].correct} key={4} myAnswer={this.state.trivia[0].d}/>
     </div>
   
    </div>
--- src/answer/answer.component.jsx
+++ src/answer/answer.component.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import './answer.styles.scss'
 
-const Answer = (props) => (
-    
-    
-    <div id={props.id} className="answers blink">{props.myAnswer1}</div>
+class Answer extends React.Component {
+        constructor(props) {
+        super(props)
+
+            this.state = {
+            backgroundColor: null
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.calculateAnswer = this.calculateAnswer.bind(this)
+    }
    
-)
+    calculateAnswer() {
+        if(this.props.id === this.props.correctAnswer) {
+            this.setState({ correctAnswer: true })
+        }
+    }
+
+    handleClick(e) {
+        // this.calculateAnswer()
+            if(this.props.id === this.props.correctAnswer){
+                this.setState({backgroundColor: 'green'})
+            }else {
+                this.setState({backgroundColor: 'red'})
+            }
+        }
+    render() {
+    
+    return (
+    <div id={this.props.id} onClick={this.handleClick} style={{backgroundColor: this.state.backgroundColor}} className="answers blink">{this.props.myAnswer}</div>
+    )
+    }
+}
 
 export default Answer
